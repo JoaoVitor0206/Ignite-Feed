4846fd61779ad534aebd20f4fc064fbabc4feefa
JoaoVitor0206 <jvrs3759@gmail.com>
2024-05-21 17:03:59 -0300

    Propriedades Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,26 +1,46 @@
+/* eslint-disable react/jsx-key */
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 import styles from './Post.module.css';
 import { Comment } from './Comment';
 import { Avatar } from './Avatar';
 
-export function Post(props) {
-  console.log(props);
+export function Post({ author, publishedAt, content }) {
+  const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+    locale: ptBR,
+  })
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale : ptBR,
+    addSuffix: true,
+  })
 
   return (
     <article className={styles.post}> 
       <header>
         <div className={styles.author}>
-          <Avatar src="https://github.com/JoaoVitor0206.png" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>João Vitor</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="16 de Maio às 16:42h" dateTime="2024-05-16 16:42:30">Publicado há 1h</time>
+        <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+          {publishedDateRelativeToNow}
+        </time>
       </header>
 
       <div className={styles.content}>
-
+        {content.map(line => {
+          if (line.type === 'paragraph') {
+            return <p>{line.content}</p>;
+          } else if (line.type === 'link') {
+            return <p><a href="#">{line.content}</a></p>
+          }
+        })}
       </div>  
 
       <form className={styles.commentForm}>
